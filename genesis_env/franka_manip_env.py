db4a0b0364a71d86510f2a0e24632fd71131e030
Quentin Clark <qtcc@cs.toronto.edu>
2025-04-07 17:35:43 -0400

    who cares

--- genesis_env/franka_manip_env.py
+++ genesis_env/franka_manip_env.py
@@ -210,6 +210,7 @@ class FrankaManipEnv:
             return 0
 
         legal_commands = ['move_x','move_y','move_z','gripper_open','gripper_close']
+        print(plan_line_by_line)
         plan_line_by_line = llm_plan.splitlines()
         for line in plan_line_by_line:
             print("EXECUTING:", line)
