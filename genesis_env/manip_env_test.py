cd1931623469b7511b0a6cf80c868e4fb5474735
Quentin Clark <qtcc@cs.toronto.edu>
2025-04-07 20:59:56 -0400

    some testing stuff

--- genesis_env/manip_env_test.py
+++ genesis_env/manip_env_test.py
@@ -1,16 +1,18 @@
 from franka_manip_env import FrankaManipEnv
 
-env = FrankaManipEnv(render_video=False,show_viewer=True,verbose=True, use_gpu=True) # change however you want
-goal_dict = {'red_cube_goal':[.25,.25,0.02],
+env = FrankaManipEnv(render_video=False,show_viewer=False,verbose=False, use_gpu=True) # change however you want
+goal_dict = {'red_cube_goal':[0,0.375,0.02],
              'blue_cube_goal':[-.25,.25,0.02],
              'yellow_cube_goal':[.25,.5,0.02],
              'green_cube_goal':[0,.5,0.02]}
 env.reset(goal_location=goal_dict)
 # correct plan
-llm_plan = 'move_x(-0.25)\nmove_y(0.25)\n\pick_block()\nmove_x(0.25)\nplace_block()'
-#llm_plan = 'move_z(1.5)\nmove_z(-1.5)\nmove_z(1.5)'
-reward = env.execute_llm_plan(llm_plan)
-print('Correct Sequence Reward:', reward)
+for i in range(8):
+    llm_plan = '\nmove_x(0.25)\npick_block()\nmove_x(-0.25)\nmove_y(0.125)\nplace_block()'
+    #llm_plan = 'move_z(1.5)\nmove_z(-1.5)\nmove_z(1.5)'
+    reward = env.execute_llm_plan(llm_plan)
+    print('Correct Sequence Reward:', reward)
+    env.reset(goal_location=goal_dict)
 # incorrect plan
 env.reset(goal_location=goal_dict)
 llm_plan = 'move_x(-0.25)\nmove_y(0.5)'
