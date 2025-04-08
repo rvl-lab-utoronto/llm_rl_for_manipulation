e1aba17370c1321d129a67b26e23410d94f518f4
Quentin Clark <qtcc@cs.toronto.edu>
2025-04-08 12:33:40 -0400

    moved camera

--- genesis_env/franka_manip_env.py
+++ genesis_env/franka_manip_env.py
@@ -78,7 +78,7 @@ class FrankaManipEnv:
                                           surface=gs.surfaces.Default(color=(0.2, 0.8, 0.2, 1.0)))
         # camera shit
         if self.render_video:
-            self.cam = self.scene.add_camera(res=(640, 480), pos = (0.25,0.75,0.5), lookat=(0.25,0.25,0), fov=30, GUI=False)
+            self.cam = self.scene.add_camera(res=(640, 480), pos = (1,0.25,1), lookat=(0.25,0.25,0), fov=30, GUI=False)
         # build
         self.scene.build()
 
