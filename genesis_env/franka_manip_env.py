ad3bf8847489df635edbdacf87b58bc4184d28c5
Quentin Clark <qtcc@cs.toronto.edu>
2025-04-08 12:23:01 -0400

    asdf

--- genesis_env/franka_manip_env.py
+++ genesis_env/franka_manip_env.py
@@ -77,7 +77,7 @@ class FrankaManipEnv:
                                           surface=gs.surfaces.Default(color=(0.2, 0.8, 0.2, 1.0)))
         # camera shit
         if self.render_video:
-            self.cam = self.scene.add_camera(res=(640, 480), pos = (1,1.5,1), lookat=(0,0,0), fov=30, GUI=False)
+            self.cam = self.scene.add_camera(res=(640, 480), pos = (-1,1.5,1), lookat=(0,0,0), fov=30, GUI=False)
         # build
         self.scene.build()
 
