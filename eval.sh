e8b2db6818a3db3cef5710506cce527ab7b4d8c5
Qentin Clark <qtcc@cs.toronto.edu>
2025-06-05 09:38:35 -0700

    updated eval script

--- eval.sh
+++ eval.sh
@@ -22,21 +22,22 @@
 
 # ### Math Dataset Scripts
 
-# # Eval for base Qwen2.5 7B Instruct model on manipulation
-# python run_eval_local_model.py \
-# --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-# --use_lora False \
-# --dataset_name 'GSMK8'
-
-# # Eval for base Qwen2.5 7B RL post-trained model on manipulation
-# python run_eval_local_model.py \
-# --base_model 'Qwen/Qwen2.5-7B-Instruct' \
-# --use_lora True \
-# --lora_name 'grpo_saved_lora2' \
-# --dataset_name 'GSMK8'
-
 # Eval for R1-reasoning distilled Qwen 7B RL post-trained model on manipulation
 python run_eval_local_model.py \
 --base_model 'unsloth/DeepSeek-R1-Distill-Qwen-7B' \
 --use_lora False \
 --dataset_name 'GSMK8'
+
+# # Eval for base Qwen2.5 7B Instruct model on manipulation
+python run_eval_local_model.py \
+--base_model 'Qwen/Qwen2.5-7B-Instruct' \
+--use_lora False \
+--dataset_name 'GSMK8'
+
+# # Eval for base Qwen2.5 7B RL post-trained model on manipulation
+python run_eval_local_model.py \
+--base_model 'Qwen/Qwen2.5-7B-Instruct' \
+--use_lora True \
+--lora_name 'grpo_saved_lora2' \
+--dataset_name 'GSMK8'
+
